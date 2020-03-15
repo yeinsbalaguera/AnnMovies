2b615b34d88a64b77cd6c7d74dddee45052b7b3d
Salgado, Anahí X.-ND <anahi.x.salgado.-nd@disney.com>
2020-03-15 17:50:40 -0500

    1. Skeleton Project

---
+++ app/src/main/java/com/example/annmovies/presentation/main/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.annmovies.presentation.main.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.annmovies.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
